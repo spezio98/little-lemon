0efd3c3952eb42568b8f8160b90628f5c498b710
spezio98 <speziale98@hotmail.it>
2025-01-11 13:33:53 +0100

    feat: added Onboarding screen and theme components
    This feature include:
    - added Onboarding screen and related composables
    - added Colors and Fonts, adapting Theme and Typography
    - added logo image
    - added strings

---
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -0,0 +1,191 @@
+package com.example.littlelemon
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.littlelemon.ui.theme.Primary
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import com.example.littlelemon.ui.theme.Secondary
+
+@Composable
+fun Onboarding(modifier: Modifier) {
+    Column(
+        //verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Logo()
+        OnboardingTitle()
+        OnboardingForm()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnboardingPreview() {
+    Onboarding(Modifier)
+}
+
+@Composable
+fun Logo() {
+    Image(
+        painter = painterResource(id = R.drawable.logo),
+        contentDescription = "Little Lemon Logo",
+        modifier = Modifier
+            .padding(vertical = 20.dp)
+            .size(width = 200.dp, height = 50.dp)
+
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LogoPreview() {
+    Logo()
+}
+
+@Composable
+fun OnboardingTitle() {
+    Text(
+        text = stringResource(R.string.onboarding_title),
+        style = MaterialTheme.typography.titleLarge,
+        textAlign = TextAlign.Center,
+        color = Color.White,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Primary)
+            .padding(vertical = 30.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnboardingTitlePreview() {
+    OnboardingTitle()
+}
+
+@Composable
+fun OnboardingForm() {
+
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 15.dp, vertical = 30.dp)
+    ) {
+        //Title
+        Text(
+            text = stringResource(R.string.onboarding_form_title),
+            style = MaterialTheme.typography.labelLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 20.dp))
+
+        //first name
+        Text(
+            text = stringResource(R.string.onboarding_first_name),
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = firstName,
+            textStyle = MaterialTheme.typography.bodySmall,
+            onValueChange = {
+                firstName = it
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp))
+
+        //last name
+        Text(
+            text = stringResource(R.string.onboarding_last_name),
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = lastName,
+            textStyle = MaterialTheme.typography.bodySmall,
+            onValueChange = {
+                lastName = it
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp))
+
+        //email
+        Text(
+            text = stringResource(R.string.onboarding_email),
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = email,
+            textStyle = MaterialTheme.typography.bodySmall,
+            onValueChange = {
+                email = it
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        //Button
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Secondary,
+                    contentColor = Primary
+                ),
+                shape = MaterialTheme.shapes.medium,
+                border = BorderStroke(width = 1.dp, color = Primary),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(16.dp)
+            ) {
+                Text(text = stringResource(R.string.onboarding_register_button_text))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnboardingFormPreview() {
+    OnboardingForm()
+}
--- app/src/main/java/com/example/littlelemon/ui/theme/Type.kt
+++ app/src/main/java/com/example/littlelemon/ui/theme/Type.kt
@@ -2,19 +2,61 @@ package com.example.littlelemon.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    //Display title
+    titleLarge = TextStyle(
+        fontFamily = markaziFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 54.sp
+    ),
+    //Sub title
+    titleMedium = TextStyle(
+        fontFamily = markaziFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 40.sp
+    ),
+    //section title, to make uppercase
+    titleSmall = TextStyle(
+        fontFamily = karlaFont,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 20.sp
+    ),
+    //Section categories
+    labelLarge = TextStyle(
+        fontFamily = karlaFont,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 16.sp
+    ),
+    //card title
+    labelMedium = TextStyle(
+        fontFamily = karlaFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    //lead text
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = karlaFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp
+    ),
+    //highlight text
+    bodyMedium = TextStyle(
+        fontFamily = karlaFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        color = Primary
+    ),
+    //paragraph text
+    bodySmall = TextStyle(
+        fontFamily = karlaFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+        color = Primary
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
